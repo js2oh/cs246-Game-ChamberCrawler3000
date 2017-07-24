7f7132f1b8aa9229178421f00860f249eadacffe
Jun Sung Oh <js2oh@uwaterloo.ca>
2017-07-24 10:55:58 -0400

    added new class playerdecorator

---
+++ item/playerdecorator.cc
@@ -0,0 +1,17 @@
+#include "player.h"
+#include "playerdecorator.h"
+#include <memory>
+
+using namespace std;
+
+PlayerDecorator::PlayerDecorator(shared_ptr<Player> p): player{p} {}
+
+int PlayerDecorator::getMaxHP() {return player->getMaxHP();}
+
+int PlayerDecorator::getCurHP() {return player->getCurHP();}
+
+int PlayerDecorator::getAtk() {return player->getAtk();}
+
+int PlayerDecorator::getDef() {return player->getDef();}
+
+shared_ptr<Player> PlayerDecorator::getPlayer() {return player;}
