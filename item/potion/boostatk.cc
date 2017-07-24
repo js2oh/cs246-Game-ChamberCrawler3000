ac867ee96492dc42b19590a68cbc2ba656406cae
Jun Sung Oh <js2oh@uwaterloo.ca>
2017-07-24 10:47:25 -0400

    need getTempAmt() initialized when the potion is created through factory since race drow can have x1.5 potion effect

---
+++ item/potion/boostatk.cc
@@ -0,0 +1,30 @@
+#include "boostatk.h"
+#include <memory>
+
+using namespace std;
+
+BoostAtk::BoostAtk(shared_ptr<Player> p): PlayerDecorator(p){}
+
+int BoostAtk::getMaxHP(){
+    int new_mhp = PlayerDecorator::getMaxHP();
+    return new_mhp;
+}
+
+int BoostAtk::getCurHP(){
+    int new_chp = PlayerDecorator::getCurHP();
+    return new_chp;
+}
+
+int BoostAtk::getAtk(){
+    int new_atk = PlayerDecorator::getAtk() + getTempAmt();
+    return new_atk;
+}
+
+int BoostAtk::getDef(){
+    int new_def = PlayerDecorator::getDef();
+    return new_def;
+}
+
+shared_ptr<Player> BoostAtk::getPlayer(){
+    return PlayerDecorator::getPlayer();
+}
