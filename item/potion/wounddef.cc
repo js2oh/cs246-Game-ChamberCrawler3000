6ef6e14d2470c76bb8998fb6ddb4405294fc64aa
Jun Sung Oh <js2oh@uwaterloo.ca>
2017-07-24 10:52:46 -0400

    Update wounddef.cc

---
+++ item/potion/wounddef.cc
@@ -0,0 +1,30 @@
+#include "wounddef.h"
+#include <memory>
+
+using namespace std;
+
+WoundDef::WoundDef(shared_ptr<Player> p): PlayerDecorator(p){}
+
+int WoundDef::getMaxHP(){
+    int new_mhp = PlayerDecorator::getMaxHP();
+    return new_mhp;
+}
+
+int WoundDef::getCurHP(){
+    int new_chp = PlayerDecorator::getCurHP();
+    return new_chp;
+}
+
+int WoundDef::getAtk(){
+    int new_atk = PlayerDecorator::getAtk();
+    return new_atk;
+}
+
+int WoundDef::getDef(){
+    int new_def = PlayerDecorator::getDef() - getTempAmt();
+    return new_def;
+}
+
+shared_ptr<Player> WoundDef::getPlayer(){
+    return PlayerDecorator::getPlayer();
+}
