6e35a6e813f99c5f492686d0426ddb19c1a5fb22
Jun Sung Oh <js2oh@uwaterloo.ca>
2017-07-24 10:50:37 -0400

    Update boostdef.h

--- item/potion/boostdef.h
+++ item/potion/boostdef.h
@@ -1,4 +1,19 @@
 #ifndef BOOSTDEF_H
 #define BOOSTDEF_H
+#include "player.h"
+#include "playerDecorator.h"
+#include <memory>
+
+using namespace std;
+
+class BoostDef: public PlayerDecorator, public Potion {
+public:
+    BoostDef(shared_ptr<Player> p);
+    int getMaxHP() override;
+    int getCurHP() override;
+    int getAtk() override;
+    int getDef() override;
+    shared_ptr<Player> getPlayer() override;
+};
 
 #endif /* BOOSTDEF_H */
