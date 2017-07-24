9611008c4301d5888804b92271a4cffaf716bcb4
Jun Sung Oh <js2oh@uwaterloo.ca>
2017-07-24 10:53:30 -0400

    Update wounddef.h

--- item/potion/wounddef.h
+++ item/potion/wounddef.h
@@ -1,4 +1,19 @@
 #ifndef WOUNDDEF_H
 #define WOUNDDEF_H
+#include "player.h"
+#include "playerDecorator.h"
+#include <memory>
+
+using namespace std;
+
+class WoundDef: public PlayerDecorator, public Potion {
+public:
+    WoundDef(shared_ptr<Player> p);
+    int getMaxHP() override;
+    int getCurHP() override;
+    int getAtk() override;
+    int getDef() override;
+    shared_ptr<Player> getPlayer() override;
+};
 
 #endif /* WOUNDDEF_H */
