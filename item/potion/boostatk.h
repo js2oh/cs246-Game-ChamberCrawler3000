79282a7a5f62fce0564bfcca809e2d1cfaad7a20
Jun Sung Oh <js2oh@uwaterloo.ca>
2017-07-24 10:48:59 -0400

    Update boostatk.h

--- item/potion/boostatk.h
+++ item/potion/boostatk.h
@@ -1,4 +1,18 @@
 #ifndef BOOSTATK_H
 #define BOOSTATK_H
+#include "player.h"
+#include "playerDecorator.h"
+#include <memory>
 
+using namespace std;
+
+class BoostAtk: public PlayerDecorator, public Potion {
+public:
+    BoostAtk(shared_ptr<Player> p);
+    int getMaxHP() override;
+    int getCurHP() override;
+    int getAtk() override;
+    int getDef() override;
+    shared_ptr<Player> getPlayer() override;
+};
 #endif /* BOOSTATK_H */
