0b5462cee315eb2b3a01c6b471fe7ef95a7b25fe
Jun Sung Oh <js2oh@uwaterloo.ca>
2017-07-24 10:52:15 -0400

    Update woundatk.h

--- item/potion/woundatk.h
+++ item/potion/woundatk.h
@@ -1,4 +1,19 @@
 #ifndef WOUNDATK_H
 #define WOUNDATK_H
+#include "player.h"
+#include "playerDecorator.h"
+#include <memory>
+
+using namespace std;
+
+class WoundAtk: public PlayerDecorator, public Potion {
+public:
+    WoundAtk(shared_ptr<Player> p);
+    int getMaxHP() override;
+    int getCurHP() override;
+    int getAtk() override;
+    int getDef() override;
+    shared_ptr<Player> getPlayer() override;
+};
 
 #endif /* WOUNDATK_H */
